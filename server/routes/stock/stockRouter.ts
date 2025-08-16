cce3258ac88e682eca5ee36558fd1f4b391498b4
Vasil Dininski <dininski@gmail.com>
2025-08-16 18:54:11 +0300

    Remove unused async

--- server/routes/stock/stockRouter.ts
+++ server/routes/stock/stockRouter.ts
@@ -3,13 +3,13 @@ import { regenerateData, getData } from "@stock-advisor/server/data";
 
 const router = Router({ strict: true });
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
   res.header("Content-type", "application/json");
   res.status(200);
   res.send(getData());
 });
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   regenerateData();
   res.status(200);
   res.send(getData());
