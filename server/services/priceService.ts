8c7bc461ddb26b0f6c51718f46bcad2cc808fd54
Vasil Dininski <dininski@gmail.com>
2025-08-16 15:09:08 +0300

    Handle descending price edge case

--- server/services/priceService.ts
+++ server/services/priceService.ts
@@ -3,20 +3,23 @@ import type { PriceResponse } from "server/routes/price/priceModel";
 
 // TODO: change return type
 // TODO: error handling
-// TODO: edge case handling, i.e. just loss
-const calculateBestPrice = (stock: Stock[]): PriceResponse => {
+const calculateBestPrice = (stock: Stock[]): PriceResponse | null => {
+  let isProfitable = false;
   if (stock.length < 2) {
-    throw new Error('Not enough data points for stock. More ticker prices necessary');
+    throw new Error(
+      "Not enough data points for stock. More ticker prices needed.",
+    );
   }
 
   let minBuy = stock[0];
   let maxProfit = 0;
   let bestBuy = stock[0];
   let bestSell = stock[1];
-
+  
   for (let i = 0; i < stock.length; i++) {
     const profit = stock[i].price - minBuy.price;
     if (profit > maxProfit) {
+      isProfitable = true;
       maxProfit = profit;
       bestBuy = minBuy;
       bestSell = stock[i];
@@ -27,17 +30,19 @@ const calculateBestPrice = (stock: Stock[]): PriceResponse => {
     }
   }
 
-  return {
-    buyInformation: {
-      price: bestBuy.price,
-      date: bestBuy.date,
-    },
-    sellInformation: {
-      price: bestSell.price,
-      date: bestSell.date,
-    },
-    profit: maxProfit,
-  };
+  return isProfitable
+    ? {
+        buyInformation: {
+          price: bestBuy.price,
+          date: bestBuy.date,
+        },
+        sellInformation: {
+          price: bestSell.price,
+          date: bestSell.date,
+        },
+        profit: maxProfit,
+      }
+    : null;
 };
 
 export { calculateBestPrice };
--- server/tests/services/priceService.test.ts
+++ server/tests/services/priceService.test.ts
@@ -13,30 +13,41 @@ describe("priceService", () => {
     it("calculates for ascending price", () => {
       const prices = generateData([1, 2, 3, 4, 5]);
       const result = calculateBestPrice(prices);
-      expect(result.profit).toEqual(4);
-      expect(result.buyInformation.date).toEqual(0);
-      expect(result.sellInformation.date).toEqual(4);
+      expect(result).not.toBe(null);
+      expect(result!.profit).toEqual(4);
+      expect(result!.buyInformation.date).toEqual(0);
+      expect(result!.sellInformation.date).toEqual(4);
     });
 
     it("calculates when best price is in the middle", () => {
       const prices = generateData([5, 6, 1, 4, 2, 3]);
       const result = calculateBestPrice(prices);
-      expect(result.profit).toEqual(3);
-      expect(result.buyInformation.date).toEqual(2);
-      expect(result.sellInformation.date).toEqual(3);
+      expect(result).not.toBe(null);
+      expect(result!.profit).toEqual(3);
+      expect(result!.buyInformation.date).toEqual(2);
+      expect(result!.sellInformation.date).toEqual(3);
     });
 
     it("calculates when best price is first transaction", () => {
       const prices = generateData([1, 100, 2, 3, 4, 5, 6, 6]);
       const result = calculateBestPrice(prices);
-      expect(result.profit).toEqual(99);
-      expect(result.buyInformation.date).toEqual(0);
-      expect(result.sellInformation.date).toEqual(1);
+      expect(result).not.toBe(null);
+
+      expect(result!.profit).toEqual(99);
+      expect(result!.buyInformation.date).toEqual(0);
+      expect(result!.sellInformation.date).toEqual(1);
     });
 
     it("throws when less than 2 prices provided", () => {
       const prices = generateData([1]);
       expect(() => calculateBestPrice(prices)).toThrow();
+    });
+
+    it("returns null when descending", () => {
+      const prices = generateData([5, 4, 3, 2, 1]);
+      const result = calculateBestPrice(prices);
+      console.log(result)
+      expect(result).toBe(null);
     });
   });
 });
