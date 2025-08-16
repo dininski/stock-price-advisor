d085a748a603207263edb81a128081c09e4fe0b9
Vasil Dininski <dininski@gmail.com>
2025-08-16 20:14:26 +0300

    Remove unused next()

--- server/routes/price/priceRouter.ts
+++ server/routes/price/priceRouter.ts
@@ -7,26 +7,23 @@ import { ApiError } from "server/util/errors";
 
 const router = Router({ strict: true });
 
-router.get(
-  "/best",
-  (req: Request, res: Response<PriceResponse | null>, next: NextFunction) => {
-    const { buyTime: buyTimeQueryParam, sellTime: sellTimeQueryParam } =
-      req.query;
-    if (!buyTimeQueryParam || !sellTimeQueryParam) {
-      throw new ApiError("Provide both a buy time and a sell time.", 400);
-    }
-
-    const buyTime = Number(buyTimeQueryParam);
-    const sellTime = Number(sellTimeQueryParam);
-
-    if (Number.isNaN(buyTime) || Number.isNaN(sellTime)) {
-      throw new ApiError("Invalid date and time format provided.", 400);
-    }
-
-    const result = calculateBestPrice(getData(), buyTime, sellTime);
-    res.status(200);
-    res.send(result);
+router.get("/best", (req: Request, res: Response<PriceResponse | null>) => {
+  const { buyTime: buyTimeQueryParam, sellTime: sellTimeQueryParam } =
+    req.query;
+  if (!buyTimeQueryParam || !sellTimeQueryParam) {
+    throw new ApiError("Provide both a buy time and a sell time.", 400);
   }
-);
+
+  const buyTime = Number(buyTimeQueryParam);
+  const sellTime = Number(sellTimeQueryParam);
+
+  if (Number.isNaN(buyTime) || Number.isNaN(sellTime)) {
+    throw new ApiError("Invalid date and time format provided.", 400);
+  }
+
+  const result = calculateBestPrice(getData(), buyTime, sellTime);
+  res.status(200);
+  res.send(result);
+});
 
 export default router;
