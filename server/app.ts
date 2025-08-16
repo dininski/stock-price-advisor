9785fa5849c0a9a380c33d2d899fe14730e82bb5
Vasil Dininski <dininski@gmail.com>
2025-08-16 17:50:09 +0300

    Update entry point and enable cors

--- server/app.ts
+++ server/app.ts
@@ -3,13 +3,18 @@ import api from "./routes";
 import express from "express";
 import morgan from "morgan";
 import compression from "compression";
+import cors from "cors";
+
+const PORT = Number.parseInt(process.env.PORT || "3030");
 
 const app = express();
 
 app.use(compression());
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use("/api/v1/", api);
 app.disable("x-powered-by");
-
-export { app };
+app.use("/api/v1/", api);
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port: ${PORT}`);
+});
