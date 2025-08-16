b9e5261a863f31e4fd27d1d33a7240a26dc61adc
Vasil Dininski <dininski@gmail.com>
2025-08-16 15:09:07 +0300

    Remove virtual modules

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,6 @@
 import "react-router";
 import api from "./routes";
 import express from "express";
-import { createRequestHandler } from "@react-router/express";
-import morgan from "morgan";
-
-const BUILD_PATH = "./build/server/index.js";
-const DEVELOPMENT = process.env.NODE_ENV === "development";
-const PORT = Number.parseInt(process.env.PORT || "3000");
 
 declare module "react-router" {
   interface AppLoadContext {
@@ -16,51 +10,4 @@ declare module "react-router" {
 
 export const app = express();
 
-app.use(
-  createRequestHandler({
-    build: () => import("virtual:react-router/server-build"),
-    getLoadContext() {
-      return {
-        VALUE_FROM_EXPRESS: "Hello from Express",
-      };
-    },
-  }),
-);
-
 app.use("/api/v1/", api);
-
-if (DEVELOPMENT) {
-  console.log("Starting development server");
-  const viteDevServer = await import("vite").then((vite) =>
-    vite.createServer({
-      server: { middlewareMode: true },
-    }),
-  );
-  app.use(viteDevServer.middlewares);
-  app.use(async (req, res, next) => {
-    try {
-      const source = await viteDevServer.ssrLoadModule("./server/app.ts");
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      return await source.app(req, res, next);
-    } catch (error) {
-      if (typeof error === "object" && error instanceof Error) {
-        viteDevServer.ssrFixStacktrace(error);
-      }
-      next(error);
-    }
-  });
-} else {
-  console.log("Starting production server");
-  app.use(
-    "/assets",
-    express.static("build/client/assets", { immutable: true, maxAge: "1y" }),
-  );
-  app.use(morgan("tiny"));
-  app.use(express.static("build/client", { maxAge: "1h" }));
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-  app.use(await import(BUILD_PATH).then((mod) => mod.app));
-}
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
