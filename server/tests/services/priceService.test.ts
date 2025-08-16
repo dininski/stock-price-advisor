fac089ef72c05a2072c41dc403df8d8b1451c418
Vasil Dininski <dininski@gmail.com>
2025-08-16 15:09:07 +0300

    Cover more cases for price service

--- server/tests/services/priceService.test.ts
+++ server/tests/services/priceService.test.ts
@@ -17,5 +17,21 @@ describe("priceService", () => {
       expect(result.buyInformation.date).toEqual(0);
       expect(result.sellInformation.date).toEqual(4);
     });
+
+    it("calculates when best price is in the middle", () => {
+      const prices = generateData([5,6,1,4,2,3]);
+      const result = calculateBestPrice(prices)
+      expect(result.profit).toEqual(3)
+      expect(result.buyInformation.date).toEqual(2)
+      expect(result.sellInformation.date).toEqual(3)
+    })
+
+    it("calculates when best price is first transaction", () => {
+      const prices = generateData([1,100, 2,3,4,5,6,6])
+      const result = calculateBestPrice(prices)
+            expect(result.profit).toEqual(99)
+      expect(result.buyInformation.date).toEqual(0)
+      expect(result.sellInformation.date).toEqual(1)
+    })
   });
 });
