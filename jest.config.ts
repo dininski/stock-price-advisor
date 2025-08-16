d99b0150ce781e8b022d58fa390b9bd4d53082ec
Vasil Dininski <dininski@gmail.com>
2025-08-16 15:09:06 +0300

    Add jest and basic test

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+// TODO: fix vs code integration
+
+/** @jest-config-loader ts-node */
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+};
+
+export default config;
---
+++ server/tests/app.test.ts
@@ -0,0 +1,5 @@
+describe("example", () => {
+    it("runs an empty test", () => {
+        expect(1).toBe(1);
+    })
+})
